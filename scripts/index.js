eec6f6bc9d54810060f5af85df71aeed447586b0
Rohit Chandra <chandrarohit91@gmail.com>
2024-10-22 12:36:12 +0530

    my first project in website development

---
+++ scripts/index.js
@@ -0,0 +1,10 @@
+/* this is comment*/
+
+//alert("Hello World");  this is single line comment
+document.querySelector("h1").style.color = "red";
+document.querySelector("h1").style.fontSize = "50px";
+document.querySelector("h1").style.fontWeight = "bold";
+document.querySelector("#onSubmit").click();
+document.querySelector("#clickMe").margin = "10px";
+document.body.style.backgroundColor = "lightblue";
+// document.querySelector("div").style.backgroundColor = "lightgreen";
